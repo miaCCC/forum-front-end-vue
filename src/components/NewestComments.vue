642a4091eb089fb977f65347cbd752ffaf86acc3
miaCCC <ta828383@gmail.com>
2022-09-16 04:18:42 +0800

    Create Navtabs components and render feeds page

---
+++ src/components/NewestComments.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      最新評論
+    </div>
+    <div class="card-body">
+      <div v-for="comment in comments"
+        :key="comment.id">
+        <h4>
+          <a href="#">
+            {{ comment.Restaurant.name }}
+          </a>
+        </h4>
+        <p> {{ comment.text }}</p>by
+        <a href="#">
+          {{ comment.User.name }}
+        </a>
+        {{ comment.createdAt | fromNow }}
+        <hr>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 先將 moment 載入進來
+import moment from 'moment'
+
+export default {
+  filters: {
+    fromNow (datetime) {
+      //判斷傳入值是否存在
+      if (!datetime) {
+        return '-'
+      }
+      // 使用 moment 提供的 fromNow 方法
+      return moment(datetime).fromNow()
+    }
+  },
+  props: {
+    comments: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
---
+++ src/components/NewestRestaurants.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      最新餐廳
+    </div>
+    <div class="card-body">
+      <!--v-for迴圈帶入restaurants資料-->
+      <div v-for="restaurant in restaurants"
+        :key="restaurant.id">
+        <h4>
+          <a href="#">{{ restaurant.name }}</a>
+          &nbsp;
+          <small>
+            <!--如果是null就顯示未分類-->
+            {{ restaurant.Category ? restaurant.Category.name : '未分類'}}
+          </small>
+        </h4>
+        <p>{{ restaurant.description }}</p>
+        {{ restaurant.createdAt | fromNow }}
+        <hr>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 先將 moment 載入進來
+import moment from 'moment'
+
+export default {
+  filters: {
+    fromNow (datetime) {
+      //判斷傳入值是否存在
+      if (!datetime) {
+        return '-'
+      }
+      // 使用 moment 提供的 fromNow 方法
+      return moment(datetime).fromNow()
+    }
+  },
+  props: {
+    restaurants: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+small {
+  font-size: 10px;
+}
+</style>
