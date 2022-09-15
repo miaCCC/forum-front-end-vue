81671f814df6e0afc4d1792580cb049cd7e00147
miaCCC <ta828383@gmail.com>
2022-09-15 17:35:59 +0800

    Initialize Project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="restaurant-forum">
+    <main
+      role="main"
+      class="mt-5 bg-white">
+        <Navbar /> <!--簡寫直接載入-->
+    </main>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Navbar //Navbar: Navbar簡寫可直接定義
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
