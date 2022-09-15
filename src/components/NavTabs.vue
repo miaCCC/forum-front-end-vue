78c2b20e766f76f6ab83f444dfade505cccc657b
miaCCC <ta828383@gmail.com>
2022-09-15 20:50:08 +0800

    Vue-router Setting

---
+++ src/components/NavTabs.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="nav nav-tabs mb-4">
+    <li class="nav-item">
+      <a
+        href="/restaurants"
+        class="nav-link"
+      >
+        首頁
+      </a>
+    </li>
+    <li class="nav-item">
+      <a
+        href="/restaurants/feeds"
+        class="nav-link"
+      >
+        最新動態
+      </a>
+    </li>
+    <li class="nav-item">
+      <a
+        href="/restaurants/top"
+        class="nav-link"
+      >
+        TOP 10 人氣餐廳
+      </a>
+    </li>
+    <li class="nav-item">
+      <a
+        href="/users/top"
+        class="nav-link"
+      >
+        美食達人
+      </a>
+    </li>
+  </ul>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,45 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
+  {//根目錄主頁為餐廳列表
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
   {
     path: '/signup',
     name: 'sign-up',
     component: () => import('../views/SignUp.vue')
   },
   {
-    path: '/signin',
-    name: 'sign-in',
-    component: SignIn
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
   },
   
     {
