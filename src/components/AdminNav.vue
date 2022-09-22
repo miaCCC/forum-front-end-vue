ad79ae33eb9ba8b445ccdfb6a80c7e303248edb4
miaCCC <ta828383@gmail.com>
2022-09-22 22:37:34 +0800

    Add AdminCategories page's function

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="mb-3">
     <h1>餐廳後台</h1>
-    <router-link to="/admin/restaurants">Restaurants</router-link> |
-    <router-link to="#">Categories</router-link> |
-    <router-link to="#">Users</router-link>
+    <router-link to="/admin/restaurants">
+    Restaurants
+    </router-link> |
+    <router-link to="/admin/categories">
+    Categories
+    </router-link> |
+    <router-link to="#">
+      Users
+      </router-link>
   </div>
 </template>
